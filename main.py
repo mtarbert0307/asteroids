05612fa9718265b549e25c8e3d38843557364265
doubtingthomist <mtarbert@smapgh.org>
2025-05-20 08:09:20 -0400

    Added a clock. Set frame rate to 60.

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():   # 
             if event.type == pygame.QUIT:  # Quit Button
                 return                     #
         
         screen.fill(000000)
+
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
         pygame.display.flip() # Call this last
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
