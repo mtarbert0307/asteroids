89a79edebb7a64ff9036f3606370247795a42524
doubtingthomist <mtarbert@smapgh.org>
2025-05-20 07:53:42 -0400

    GUI operational. Added .gitignore containing venv/ and __pycache__/

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():   # 
+            if event.type == pygame.QUIT:  # Quit Button
+                return                     #
+        
+        screen.fill(000000)
+        pygame.display.flip() # Call this last
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
