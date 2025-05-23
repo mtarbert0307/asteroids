e4bf7d9bec52198a0994dabd3452668475b03c5c
doubtingthomist <mtarbert@smapgh.org>
2025-05-23 19:31:40 -0400

    Added an Asteroid class and an AsteroidField class. Updated main to spawn asteroids.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -8,10 +10,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -19,11 +28,12 @@ def main():
                 return
 
         screen.fill("black")
-        #player.update(dt)
-        #player.draw(screen)
+        
         updatable.update(dt)
+        
         for object in drawable:
             object.draw(screen)
+        
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
